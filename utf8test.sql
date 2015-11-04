a13d4cc1a68fa322d95e64d1dc5eee4d7fd69de5
Vick Khera <vivek@khera.org>
2015-11-04 16:43:14 -0500

    make the test file use postgres E-strings to it is obvious what is being stored byte for byte.

--- utf8test.sql
+++ utf8test.sql
@@ -6,7 +6,7 @@ SET standard_conforming_strings = on;
 DROP TABLE IF EXISTS t1;
 CREATE TABLE t1 (t_id SERIAL PRIMARY KEY, t_data TEXT, t_json JSONB);
 
-INSERT INTO t1 VALUES (DEFAULT, 'CrÌ†çud', NULL);
-INSERT INTO t1 VALUES (DEFAULT, 'TaÃÄst', '{"mykey": "T√†st"}');
+INSERT INTO t1 VALUES (DEFAULT, E'Cr\355\240\215ud', NULL);
+INSERT INTO t1 VALUES (DEFAULT, E'Ta\314\200st', E'{"mykey": "T\303\240st"}');
 INSERT INTO t1 VALUES (DEFAULT, 'Clean as a whistle', '{"mykey": "Howdy"}');
-INSERT INTO t1 VALUES (DEFAULT, 'Funkˇ Letters', '{"mykey": "TaÃÄrp"}');
+INSERT INTO t1 VALUES (DEFAULT, E'Funk\377 Letters', E'{"mykey": "Ta\314\200rp"}');
